ac6c96b1b9a0b09f24dd9673a3e02e8b9d3ab3af
Pattapol Sirimangklanurak <pattapolsirimangklanurak@Pattapols-MacBook-Air.local>
2026-07-17 15:25:10 +0700

    add table creation definition

--- snowflake/setup.sql
+++ snowflake/setup.sql
@@ -13,4 +13,18 @@ CREATE DATABASE IF NOT EXISTS mtg_db
 CREATE SCHEMA IF NOT EXISTS mtg_db.curated
 ;
 
-
+--Table:
+CREATE TABLE IF NOT EXISTS mtg_db.curated.card_price_features (
+    oracle_id STRING,
+    name STRING,
+    set_code STRING,
+    released_at DATE,
+    original_release_date DATE,
+    printing_age_days INTEGER,
+    is_reserved_list BOOLEAN,
+    edhrec_rank INTEGER,
+    price_usd FLOAT,
+    log_price_usd FLOAT,
+    loaded_at TIMESTAMP_NTZ DEFAULT CURRENT_TIMESTAMP()
+)
+;
